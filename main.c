7f295f1b8105bf2f1c4de2f6e17049d33bc7da98
Sana Mohiuddin <smohi002@ucr.edu>
2021-01-19 22:44:03 -0800

    init & used ctx

--- main.c
+++ main.c
@@ -3,38 +3,33 @@
 //#include <md5.c>
 
 int main(int argc, char* argv[]) {
-//fflush(stdout);
-printf("wtf0\n");
-fflush(stdout);
   const char stringnum[64] = "abcdefghijklmnopqrstuvwxyz";
-  char ret[9] = {0};
-  char salt[10] = "hfT7jp2q";
+  char rt[9] = {0};
+  char salt[10] = "4fTgjp6q";
   char pass[40] = {0};
   int i, j, k, l, m, n;
-printf("wtf\n");
-fflush(stdout);
   for(i = 0; i < strlen(stringnum); i = i + 1){
-    ret[0] = stringnum[i];
+    rt[0] = stringnum[i];
     for(j = 0; j < strlen(stringnum); j = j + 1){
-      ret[1] = stringnum[j];
+      rt[1] = stringnum[j];
       for(k = 0; k < strlen(stringnum); k = k + 1){
-	ret[2] = stringnum[k];
+	rt[2] = stringnum[k];
 	for(l = 0; l < strlen(stringnum); l = l + 1){
-	  ret[3] = stringnum[l];
+	  rt[3] = stringnum[l];
 	  for(m = 0; m < strlen(stringnum); m = m + 1){
-	    ret[4] = stringnum[m];
+	    rt[4] = stringnum[m];
 	    for(n = 0; n < strlen(stringnum); n = n + 1){
-	      ret[5] = stringnum[n];
+	      rt[5] = stringnum[n];
+  printf("wtf before func call\n");
+  fflush(stdout);
+              char newCrypt = md5_crypt(rt, salt);
   printf("wtf before strcpy\n");
   fflush(stdout);
-  // char newCrypt = md5_crypt(ret, salt);
-  printf("wtf func call\n");
-  fflush(stdout);
-  //	      strcpy(pass, &newCrypt);
+	      strcpy(pass, &newCrypt);
   printf("wtf after strcpy\n");
   fflush(stdout);
-	      if ("wPwz7GC6xLt9eQZ9eJkaq." == pass) printf("Found password: %d", pass);
-	      printf(ret);
+	      if ("2bBqhtSK.CKnOMysAG1BE0" == pass) printf("Found password: %d", pass);
+	      printf(rt);
               printf("\n");
 	    }
 	  }
@@ -42,7 +37,6 @@ fflush(stdout);
       }
     }
   }
-    
   
   return 0;
 }
