a6410f62333519c0d63025c67d41e410d3a7411c
Sana Mohiuddin <smohi002@ucr.edu>
2021-01-21 22:43:19 -0800

    Deleted error messages

--- main.c
+++ main.c
@@ -4,32 +4,26 @@
 
 int main(int argc, char* argv[]) {
   const char stringnum[64] = "abcdefghijklmnopqrstuvwxyz";
-  char rt[9] = {0};
+  char rtrn[9] = {0};
   char salt[10] = "4fTgjp6q";
   char pass[40] = {0};
   int i, j, k, l, m, n;
   for(i = 0; i < strlen(stringnum); i = i + 1){
-    rt[0] = stringnum[i];
+    rtrn[0] = stringnum[i];
     for(j = 0; j < strlen(stringnum); j = j + 1){
-      rt[1] = stringnum[j];
+      rtrn[1] = stringnum[j];
       for(k = 0; k < strlen(stringnum); k = k + 1){
-	rt[2] = stringnum[k];
+	rtrn[2] = stringnum[k];
 	for(l = 0; l < strlen(stringnum); l = l + 1){
-	  rt[3] = stringnum[l];
+	  rtrn[3] = stringnum[l];
 	  for(m = 0; m < strlen(stringnum); m = m + 1){
-	    rt[4] = stringnum[m];
+	    rtrn[4] = stringnum[m];
 	    for(n = 0; n < strlen(stringnum); n = n + 1){
-	      rt[5] = stringnum[n];
-  printf("wtf before func call\n");
-  fflush(stdout);
-              char newCrypt = md5_crypt(rt, salt);
-  printf("wtf before strcpy\n");
-  fflush(stdout);
+	      rtrn[5] = stringnum[n];
+              char newCrypt = md5_crypt(rtrn, salt);
 	      strcpy(pass, &newCrypt);
-  printf("wtf after strcpy\n");
-  fflush(stdout);
 	      if ("2bBqhtSK.CKnOMysAG1BE0" == pass) printf("Found password: %d", pass);
-	      printf(rt);
+              printf(rtrn);
               printf("\n");
 	    }
 	  }
